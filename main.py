0adac946554b433d16f72943301d1aab60479bea
Henry Lee <dailychoices@lean35.com>
2021-02-07 10:27:48 -0500

    init commits

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def print_hi():
+    print("hello world")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
