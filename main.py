81e09a69b6df3678f2e5fc93daba10f730bcb513
Henry Lee <dailychoices@lean35.com>
2021-02-07 10:39:19 -0500

    2nd commits with messages

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+# This is a sample Python script.
 
-def print_hi():
-    print("hello world")
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import requests, json
+
+
+#python request examples
+#https://www.pythonforbeginners.com/requests/using-requests-in-python
+
+#import json
+#url = 'https://api.github.com/some/endpoint'
+#payload = {'some': 'data'}
+#headers = {'content-type': 'application/json'}
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    print_hi('PyCharm')
+    github_url = "https://api.github.com/user/repos"
+    data = json.dumps({'name': 'test', 'description': 'some test repo'})
+    r = requests.post(github_url, data, auth=('user', '*****'))
+    print(r.json)
+    print("hello world")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
